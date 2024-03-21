8941ba89bfd89cabe9f40cc6aff3d7599c86ce57
David Riccitelli <david@wordlift.io>
2024-03-21 17:58:21 +0200

    test GH action

--- .bin/publish.sh
+++ .bin/publish.sh
@@ -2,8 +2,10 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-INITIAL_HASH=$(cat seovoc.owx.md5)
-CURRENT_HASH=$(md5sum seovoc.owx)
+echo "Working in $SCRIPT_DIR..."
+
+INITIAL_HASH=$(cat $SCRIPT_DIR/../seovoc.owx.md5)
+CURRENT_HASH=$(md5sum $SCRIPT_DIR/../seovoc.owx)
 
 if [[ "$INITIAL_HASH" == "$CURRENT_HASH" ]]; then
     echo "seovoc.owx unchanged." && exit 0
@@ -20,7 +22,7 @@ else
     docker run --rm -v "${SCRIPT_DIR}:/work" obolibrary/robot \
         robot convert --input "/work/seovoc.owx" --format ttl --output "/work/seovoc.ttl"
 
-    rm seovoc.owx.md5 && echo $CURRENT_HASH >seovoc.owx.md5
+    rm $SCRIPT_DIR/../seovoc.owx.md5 && echo $CURRENT_HASH >$SCRIPT_DIR/../seovoc.owx.md5
 
     git commit -m "update seovoc.owl and seovoc.ttl and hash file" \
         $SCRIPT_DIR/../seovoc.owx.md5 $SCRIPT_DIR/../seovoc.owl $SCRIPT_DIR/../seovoc.ttl
