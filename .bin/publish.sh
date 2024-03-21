b1c09d78144b1c8c1e53598c7d30a95f971b427b
David Riccitelli <david@wordlift.io>
2024-03-21 17:31:39 +0200

    test GH action

--- .bin/publish.sh
+++ .bin/publish.sh
@@ -3,9 +3,13 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 if $(git diff HEAD~1 --exit-code "$SCRIPT_DIR/../seovoc.owl" > /dev/null); then
-    echo "seovoc.owx not changed."
+    echo "seovoc.owx not changed." && exit 0
 else
     echo "seovoc.owx changed, regenerating owl and tll files."
+
+    echo "*** Configuring git..."
+    git config user.name github-actions
+    git config user.email github-actions@github.com
 
     docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
         robot convert --input "/work/seovoc.owx" --format owl --output "/work/seovoc.owl"
