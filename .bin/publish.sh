c6b69d9af644124f49ab64862138b5488c9f77a8
David Riccitelli <david@wordlift.io>
2024-03-21 17:26:43 +0200

    initial commit

--- .bin/publish.sh
+++ .bin/publish.sh
@@ -7,8 +7,11 @@ if $(git diff HEAD~1 --exit-code "$SCRIPT_DIR/../seovoc.owl" > /dev/null); then
 else
     echo "seovoc.owx changed, regenerating owl and tll files."
 
-    $SCRIPT_DIR/robot convert --input "$SCRIPT_DIR/../seovoc.owx" --format owl --output "$SCRIPT_DIR/../seovoc.owl"
-    $SCRIPT_DIR/robot convert --input "$SCRIPT_DIR/../seovoc.owx" --format ttl --output "$SCRIPT_DIR/../seovoc.ttl"
+    docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
+        robot convert --input "/work/seovoc.owx" --format owl --output "/work/seovoc.owl"
+
+    docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
+        robot convert --input "/work/seovoc.owx" --format ttl --output "/work/seovoc.ttl"
 
     git commit -m "update seovoc.owl and seovoc.ttl" $SCRIPT_DIR/../seovoc.owl $SCRIPT_DIR/../seovoc.ttl
 
