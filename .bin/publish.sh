f1c45a9ea86f99d7d67c62726d84a251159452a6
David Riccitelli <david@wordlift.io>
2024-03-21 17:43:37 +0200

    test GH action

--- .bin/publish.sh
+++ .bin/publish.sh
@@ -2,23 +2,17 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-if $(git diff HEAD~1 --exit-code "$SCRIPT_DIR/../seovoc.owl" > /dev/null); then
-    echo "seovoc.owx not changed." && exit 0
-else
-    echo "seovoc.owx changed, regenerating owl and tll files."
+echo "*** Configuring git..."
+git config user.name github-actions
+git config user.email github-actions@github.com
 
-    echo "*** Configuring git..."
-    git config user.name github-actions
-    git config user.email github-actions@github.com
+docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
+    robot convert --input "/work/seovoc.owx" --format owl --output "/work/seovoc.owl"
 
-    docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
-        robot convert --input "/work/seovoc.owx" --format owl --output "/work/seovoc.owl"
+docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
+    robot convert --input "/work/seovoc.owx" --format ttl --output "/work/seovoc.ttl"
 
-    docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
-        robot convert --input "/work/seovoc.owx" --format ttl --output "/work/seovoc.ttl"
+git commit -m "update seovoc.owl and seovoc.ttl" $SCRIPT_DIR/../seovoc.owl $SCRIPT_DIR/../seovoc.ttl
 
-    git commit -m "update seovoc.owl and seovoc.ttl" $SCRIPT_DIR/../seovoc.owl $SCRIPT_DIR/../seovoc.ttl
-
-    echo "*** Pushing changes..."
-    git push
-fi
+echo "*** Pushing changes..."
+git push
