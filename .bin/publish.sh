ab74d720bd903606dbc1d6818b4a4494ea9bb944
David Riccitelli <david@wordlift.io>
2024-03-21 17:52:45 +0200

    test GH action

--- .bin/publish.sh
+++ .bin/publish.sh
@@ -2,17 +2,30 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-echo "*** Configuring git..."
-git config user.name github-actions
-git config user.email github-actions@github.com
+INITIAL_HASH=$(cat seovoc.owx.md5)
+CURRENT_HASH=$(md5sum seovoc.owx)
 
-docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
-    robot convert --input "/work/seovoc.owx" --format owl --output "/work/seovoc.owl"
+if [[ "$INITIAL_HASH" == "$CURRENT_HASH" ]]; then
+    echo "seovoc.owx unchanged." && exit 0
+else
+    echo "MD5 hash has changed!"
 
-docker run --rm -v "${SCRIPTDIR}:/work" obolibrary/robot \
-    robot convert --input "/work/seovoc.owx" --format ttl --output "/work/seovoc.ttl"
+    echo "*** Configuring git..."
+    git config user.name github-actions
+    git config user.email github-actions@github.com
 
-git commit -m "update seovoc.owl and seovoc.ttl" $SCRIPT_DIR/../seovoc.owl $SCRIPT_DIR/../seovoc.ttl
+    docker run --rm -v "${SCRIPT_DIR}:/work" obolibrary/robot \
+        robot convert --input "/work/seovoc.owx" --format owl --output "/work/seovoc.owl"
 
-echo "*** Pushing changes..."
-git push
+    docker run --rm -v "${SCRIPT_DIR}:/work" obolibrary/robot \
+        robot convert --input "/work/seovoc.owx" --format ttl --output "/work/seovoc.ttl"
+
+    rm seovoc.owx.md5 && echo $CURRENT_HASH >seovoc.owx.md5
+
+    git commit -m "update seovoc.owl and seovoc.ttl and hash file" \
+        $SCRIPT_DIR/../seovoc.owx.md5 $SCRIPT_DIR/../seovoc.owl $SCRIPT_DIR/../seovoc.ttl
+
+    echo "*** Pushing changes..."
+    git push
+
+fi
