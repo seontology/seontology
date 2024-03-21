28db087b1d69170c36fa3568d9780b3465bfe706
David Riccitelli <david@wordlift.io>
2024-03-21 18:30:45 +0200

    format doc

--- .bin/publish.sh
+++ .bin/publish.sh
@@ -24,7 +24,8 @@ else
 
     rm $SCRIPT_DIR/../seovoc.owx.md5 && echo $CURRENT_HASH >$SCRIPT_DIR/../seovoc.owx.md5
 
-    git add -A ; git commit -m "update seovoc.owl and seovoc.ttl and hash file" -a
+    git add -A
+    git commit -m "update seovoc.owl and seovoc.ttl and hash file" -a
 
     echo "*** Pushing changes..."
     git push
